28d6a333c4f84199c9205e4417ecbfa0cfcb0e93
souhup <souhup@gmail.com>
2020-08-03 22:07:36 +0800

    Update: 两数之和

--- leetcode/1/1-two-sum.go
+++ leetcode/1/1-two-sum.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 /*
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
@@ -22,12 +22,14 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	var tmp = make(map[int]int, len(nums))
-	for pos, value := range nums {
-		if pos2, ok := tmp[target-value]; ok {
-			return []int{pos2, pos}
+	var indexMap = make(map[int]int, len(nums))
+	for index, num := range nums {
+		priIndex, ok := indexMap[target-num]
+		if ok {
+			return []int{priIndex, index}
+		} else {
+			indexMap[num] = index
 		}
-		tmp[value] = pos
 	}
 	return nil
 }
