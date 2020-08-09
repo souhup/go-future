ea047a94b54899755923df369d6b89bfa63219fa
souhup <souhup@gmail.com>
2020-08-09 21:46:38 +0800

    Add: 反转链表

---
+++ leetcode/206/206-reverse-linked-list.go
@@ -0,0 +1,59 @@
+/*
+
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+*/
+package main
+
+import "fmt"
+
+func main() {
+	header := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	list := reverseList(header)
+	for h := list; h != nil; h = h.Next {
+		fmt.Println(h.Val)
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	node := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return node
+}
