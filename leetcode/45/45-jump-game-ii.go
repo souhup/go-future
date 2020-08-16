c2254dc704a7b13d2b86237bd55ceb68c1feebd4
souhup <souhup@gmail.com>
2020-08-16 15:42:11 +0800

    Add: 跳跃游戏 II

---
+++ leetcode/45/45-jump-game-ii.go
@@ -0,0 +1,46 @@
+/*
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+示例:
+
+输入: [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+    从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+说明:
+
+假设你总是可以到达数组的最后一个位置。
+*/
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	//nums := []int{3, 2, 1}
+	fmt.Println(jump(nums))
+}
+
+func jump(nums []int) int {
+	maxPos, end, jumpCount := 0, 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		maxPos = max(maxPos, i+nums[i])
+		if i == end {
+			jumpCount++
+			end = maxPos
+		}
+	}
+	return jumpCount
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
